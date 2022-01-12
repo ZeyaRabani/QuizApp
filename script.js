12d55fb65615d4f57eac16ef146f620ace18c6c5
ZeyaRabani <ziyarabani@hotmail.co.uk>
2022-01-12 14:14:17 +0000

    Finished all of it, may need to add more questions as there are only 2 right now.

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
-let shuffledQuestions, currentQuestionIndex
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
+let shuffledQuestions, currentQuestionIndex
+
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
-    console.log('start')
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
-
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
@@ -36,21 +40,63 @@ function showQuestion(question) {
 }
 
 function resetState() {
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-            (answersButtonsElement.firstChild)
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
 
-function selectAnswer() {
-
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
 }
 
-const questions = [{
-    question: "what is 1+1?",
-    answers: [
-        { text: '4', currect: true },
-        { text: '22', correct: false }
-    ]
-}]
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+// const questions = [
+//     {
+//         question: 'What is 2 + 2?',
+//         answers: [
+const questions = [
+    {
+        question: "what is 1+1?",
+        answers: [
+            { text: '2', correct: true },
+            { text: '22', correct: false },
+            { text: '21', correct: false },
+            { text: '12', correct: false }
+        ],
+    },
+    {
+        question: 'How many miles is the earth by diameter?',
+        answers: [
+            { text: '8917', correct: false },
+            { text: '6917', correct: false },
+            { text: '7917', correct: true },
+            { text: '9917', correct: false }
+        ]
+    }
+]
